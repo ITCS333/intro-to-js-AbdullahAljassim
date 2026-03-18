ba0eaa748bafcd7687998fcb9dfda2430277c56c
Abdullah Waleed Aljassim <202310963@stu.uob.edu.bh>
2026-03-18 13:47:13 +0300

    implemented 5 functions in JS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  */
 function sum(a, b) {
   // TODO: Implement this function.
+  return a + b;
 }
 
 /**
@@ -13,6 +14,7 @@ function sum(a, b) {
  */
 function reverseString(str) {
   // TODO: Implement this function.
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -21,6 +23,15 @@ function reverseString(str) {
  */
 function findLargest(numbers) {
   // TODO: Implement this function.
+  if (numbers.length === 0)
+    return null;
+  let max = numbers[0];
+  for (let i=1; i<numbers.length; i++){
+    if (numbers[i] > max){
+      max = numbers[i];
+    }
+  }
+  return max;
 }
 
 /**
@@ -31,6 +42,9 @@ function findLargest(numbers) {
  */
 function isPalindrome(str) {
   // TODO: Implement this function.
+  const cleanStr = str.toLowerCase();
+  const reversedStr = [...cleanStr].reverse().join('');
+  return cleanStr === reversedStr;
 }
 
 /**
@@ -39,6 +53,7 @@ function isPalindrome(str) {
  */
 function filterEvenNumbers(numbers) {
   // TODO: Implement this function.
+  return numbers.filter(num => num % 2 === 0);
 }
 
 
